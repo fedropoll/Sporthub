a15c89f1f2c5588b0eea8ea109374b99092c5588
fedropoll <arstanbekovasil10@gmail.com>
2025-09-09 19:29:49 +0600

    asdfghj

--- Sporthub/settings.py
+++ Sporthub/settings.py
@@ -19,12 +19,15 @@ DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
 
+
 # ALLOWED_HOSTS
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 if not ALLOWED_HOSTS or ALLOWED_HOSTS == ['']:
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
-
+# ======================
+# База данных
+# ======================
 if os.getenv("USE_SQLITE", "False") == "True":
     DATABASES = {
         'default': {
@@ -34,16 +37,12 @@ if os.getenv("USE_SQLITE", "False") == "True":
     }
 else:
     DATABASE_URL = os.getenv('DATABASE_URL')
-    if DATABASE_URL:
-        DATABASES = {
-            'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
-        }
-    else:
+    if not DATABASE_URL:
         raise Exception("DATABASE_URL не задан в переменных окружения!")
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
 
-# ======================
-# Email
-# ======================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
