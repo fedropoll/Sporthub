1c112b5d4302cb153a4f0c92ded40e9a3e5292a1
unknown <you@example.com>
2025-09-10 16:34:29 +0600

    добавил путь

--- Sporthub/urls.py
+++ Sporthub/urls.py
@@ -3,10 +3,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework_simplejwt.views import (
-    TokenRefreshView, # Используем только TokenRefreshView
-)
-
+from django.views.generic import RedirectView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,4 +26,5 @@ urlpatterns = [
     # Swagger Documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
 ]
