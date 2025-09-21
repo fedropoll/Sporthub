3259cec70e6a9f9a13cb5d4a34e5eccfcae99469
fedropoll <arstanbekovasil10@gmail.com>
2025-09-21 14:03:12 +0600

    a

--- users/admin.py
+++ users/admin.py
@@ -6,8 +6,3 @@ admin.site.register(PasswordResetCode)
 admin.site.register(ClassSchedule)
 admin.site.register(Joinclub)
 admin.site.register(Attendance)
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role', 'phone_number')
-    list_editable = ('role',)
